99f53fb25cceb0d0544776b03bce2f8d461895fb
1111 <aaa>
2025-01-16 16:14:26 +0900

    ㅊ

--- src/page/detail.page.tsx
+++ src/page/detail.page.tsx
@@ -41,54 +41,72 @@ const Detail = () => {
                             <strong className="franNm">{ apprData.franNm }</strong>
                             <div className="ext_info">
                                 <span className="date">
-    
+                                    { apprData.apprDt + ' ' + apprData.apprTm }
                                 </span>
                                 <p className="card_info">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
+                                    <span>{ apprData.fmlYn ? '가족' : '본인' }</span>
+                                    <span>
+                                    {
+                                        apprData.brandCd === 'V' ?
+                                        <img src="../image/visa.png" />
+                                        : apprData.brandCd === 'M' ?
+                                        <img src="../image/master.png" />
+                                        : null
+                                    }
+                                    </span>
+                                    <span>{ apprData.cardNo }</span>
                                 </p>
                             </div>
                         </div>
                         <div className="receipt_body">
                             <div className="box_dl">
                                 <dl>
+                                    <dt>이용금액</dt>
+                                    <dd>
+                                        <span className="pay_amt">
+                                            { apprData.payAmt.toLocaleString() }원
+                                        </span>
+                                    </dd>
+                                </dl>
+                            </div>
+                            <div className="box_dl">
+                                <dl>
                                     <dt>승인번호</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.apprNo }</span></dd>
                                 </dl>
                                 <dl>
                                     <dt>매입상태</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.status }</span></dd>
                                 </dl>
                                 <dl>
                                     <dt>거래일자</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.apprDt + ' ' + apprData.apprTm }</span></dd>
                                 </dl>
                                 <dl>
                                     <dt>이용구분</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.payTy }</span></dd>
                                 </dl>
                             </div>
                             <div className="box_dl">
                                 <dl>
                                     <dt>카드종류</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.cardType }</span></dd>
                                 </dl>
                                 <dl>
                                     <dt>거래유형</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.trsTpNm }</span></dd>
                                 </dl>
                                 <dl>
                                     <dt>공급가액</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.aprAmt.toLocaleString() }</span></dd>
                                 </dl>
                                 <dl>
                                     <dt>부가세</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.addAmt.toLocaleString() }</span></dd>
                                 </dl>
                                 <dl>
                                     <dt>봉사료</dt>
-                                    <dd><span></span></dd>
+                                    <dd><span>{ apprData.serviceAmt.toLocaleString() }</span></dd>
                                 </dl>
                             </div>
                             <div className="btn_wrap">
